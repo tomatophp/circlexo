48600e2e7042a09ee2af4c707b8089dc47db75a7
Fady Mondy <info@3x1.io>
2024-03-27 20:43:12 +0200

    update home style

--- Modules/CircleXO/resources/views/components/parts/avatar.blade.php
+++ Modules/CircleXO/resources/views/components/parts/avatar.blade.php
@@ -1,11 +1,13 @@
-@if($item->account->avatar)
-<div  class="w-8 h-8 rounded-full bg-zinc-800 border border-zinc-700">
-    <img src="{{ $item->account->avatar }}" class="w-8 h-8 rounded-full object-cover" alt="avatar">
-</div>
-@else
-    <div  class="w-8 h-8 rounded-full bg-zinc-800 border border-zinc-700">
-        <div class="flex flex-col justify-center items-center text-center h-full">
-            <i class="bx bxs-user text-5xl text-zinc-500"></i>
+<x-splade-link :href="url($item->account->username)">
+    @if($item->account->avatar)
+        <div  class="w-8 h-8 rounded-full bg-zinc-800 border border-zinc-700">
+            <img src="{{ $item->account->avatar }}" class="w-8 h-8 rounded-full object-cover" alt="avatar">
         </div>
-    </div>
-@endif
+    @else
+        <div  class="w-8 h-8 rounded-full bg-zinc-800 border border-zinc-700">
+            <div class="flex flex-col justify-center items-center text-center h-full">
+                <i class="bx bxs-user text-5xl text-zinc-500"></i>
+            </div>
+        </div>
+    @endif
+</x-splade-link>
