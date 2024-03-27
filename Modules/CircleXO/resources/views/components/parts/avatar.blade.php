b3049a6c135acbc65b05e42a1bb3cf94624afecd
Fady Mondy <info@3x1.io>
2024-03-27 20:59:31 +0200

    fix avatar icon

--- Modules/CircleXO/resources/views/components/parts/avatar.blade.php
+++ Modules/CircleXO/resources/views/components/parts/avatar.blade.php
@@ -6,7 +6,7 @@
     @else
         <div  class="w-8 h-8 rounded-full bg-zinc-800 border border-zinc-700">
             <div class="flex flex-col justify-center items-center text-center h-full">
-                <i class="bx bxs-user text-5xl text-zinc-500"></i>
+                <i class="bx bxs-user text-zinc-500"></i>
             </div>
         </div>
     @endif
